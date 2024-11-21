d071878c78512562050ee22ed90694a20c7d96f5
Abdullo <147158343+Abdushahidov07@users.noreply.github.com>
2024-11-21 17:25:01 +0500

    new change for server

--- server/settings.py
+++ server/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+xh!!diy09)%fg#-(r&@r$df34s%+0nt#a(5$&76^d@$_@a7f!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
