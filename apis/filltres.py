ffdba280c2a936398563cb3e39806e9eb034bdaa
Abdullo <147158343+Abdushahidov07@users.noreply.github.com>
2024-11-21 17:12:33 +0500

    new requirements

--- apis/filltres.py
+++ apis/filltres.py
@@ -3,7 +3,6 @@ from .models import Bill
 
 class BillFilter(django_filters.FilterSet):
     is_active = django_filters.BooleanFilter(field_name='is_active', lookup_expr='exact', initial=True)
-
     class Meta:
         model = Bill
         fields = ['table', 'castumername', 'is_active']
